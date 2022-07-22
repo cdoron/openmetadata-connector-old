cbeb09e21f16a2ff612be5dba2a9b3f65acd29b9
Doron Chen <cdoron@il.ibm.com>
2022-07-22 16:34:51 +0300

    check if service already found
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- openmetadata_connector_service.go
+++ openmetadata_connector_service.go
@@ -253,8 +253,26 @@ func (s *OpenMetadataApiService) CreateAsset(ctx context.Context,
 
 	c := s.getOpenMetadataClient()
 
+	connectionProperties := createAssetRequest.Details.GetConnection().AdditionalProperties["mysql"].(map[string]interface{})
 	// Let us begin with checking whether the database service already exists
-	// XXXXXXXXX
+	var foundService client.DatabaseService
+
+	serviceList, r, err := c.DatabaseServiceApi.ListDatabaseServices(ctx).Execute()
+	for _, service := range serviceList.Data {
+		found := true
+		// XXXX - Check type of service (for instance "mysql")
+		for property, value := range connectionProperties {
+			if service.Connection.Config[property] != value {
+				found = false
+				break
+			}
+		}
+		if found {
+			foundService = service
+			break
+		}
+	}
+	fmt.Fprintln(os.Stderr, foundService)
 
 	// If does not exist, let us create database service
 	connection := client.NewDatabaseConnection()
