9e1c719a13aa8747cd4a30eb408572a3cc804c3a
Doron Chen <cdoron@il.ibm.com>
2022-07-23 10:35:23 +0300

    add openmetadata_connector_helper.go
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

---
+++ openmetadata_connector_helper.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	client "github.com/fybrik/datacatalog-go-client"
+	models "github.com/fybrik/datacatalog-go-models"
+)
+
+func (s *OpenMetadataApiService) findService(ctx context.Context,
+	c *client.APIClient,
+	createAssetRequest models.CreateAssetRequest) bool {
+	connectionProperties := createAssetRequest.Details.GetConnection().AdditionalProperties["mysql"].(map[string]interface{})
+	// Let us begin with checking whether the database service already exists
+	var foundService client.DatabaseService
+
+	serviceList, _, err := c.DatabaseServiceApi.ListDatabaseServices(ctx).Execute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Service does not exist yet")
+		return false
+	}
+	for _, service := range serviceList.Data {
+		found := true
+		// XXXX - Check type of service (for instance "mysql")
+		for property, value := range connectionProperties {
+			if service.Connection.Config[property] != value {
+				found = false
+				break
+			}
+		}
+		if found {
+			foundService = service
+			break
+		}
+	}
+	fmt.Fprintln(os.Stderr, foundService)
+	return true
+}
--- openmetadata_connector_service.go
+++ openmetadata_connector_service.go
@@ -253,26 +253,7 @@ func (s *OpenMetadataApiService) CreateAsset(ctx context.Context,
 
 	c := s.getOpenMetadataClient()
 
-	connectionProperties := createAssetRequest.Details.GetConnection().AdditionalProperties["mysql"].(map[string]interface{})
-	// Let us begin with checking whether the database service already exists
-	var foundService client.DatabaseService
-
-	serviceList, r, err := c.DatabaseServiceApi.ListDatabaseServices(ctx).Execute()
-	for _, service := range serviceList.Data {
-		found := true
-		// XXXX - Check type of service (for instance "mysql")
-		for property, value := range connectionProperties {
-			if service.Connection.Config[property] != value {
-				found = false
-				break
-			}
-		}
-		if found {
-			foundService = service
-			break
-		}
-	}
-	fmt.Fprintln(os.Stderr, foundService)
+	s.findService(ctx, c, createAssetRequest)
 
 	// If does not exist, let us create database service
 	connection := client.NewDatabaseConnection()
