0ac9d02787da119b73db96c97a4df5cee2981fdf
Doron Chen <cdoron@il.ibm.com>
2022-08-14 14:55:12 +0300

    comments for utils.go
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- utils/utils.go
+++ utils/utils.go
@@ -13,15 +13,20 @@ func AppendStrings(a string, b string) string {
 	}
 }
 
+// If the tag is in the Fybrik tag category (e.g. "Fybrik.PII"), remove the
+// "Fybrik." prefix. Otherwise, do nothing
 func StripTag(tag string) string {
 	return strings.TrimPrefix(tag, "Fybrik.")
 }
 
 func UpdateCustomProperty(customProperties map[string]interface{}, orig map[string]interface{}, key string, value *string) {
+	// update customProperties only if there is a new value
 	if value != nil && *value != "" {
 		customProperties[key] = *value
 		return
 	}
+
+	// if there is no new value, revert to original value
 	if v, ok := orig[key]; ok && v != "" {
 		customProperties[key] = v
 	}
@@ -29,6 +34,7 @@ func UpdateCustomProperty(customProperties map[string]interface{}, orig map[stri
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+// create random string of letters of length n
 func RandStringBytes(n int) string {
 	b := make([]byte, n)
 	for i := range b {
